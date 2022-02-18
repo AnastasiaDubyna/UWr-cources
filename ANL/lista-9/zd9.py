aab201e1df7902f543cf6424c1b0e33c05ac5f50
AnastasiaDubyna <nastyaorlova5@gmail.com>
2022-02-18 19:50:59 +0100

    added ANL solutions

--- ANL/lista-9/zd9.py
+++ ANL/lista-9/zd9.py
@@ -1,25 +1,84 @@
-import matplotlib.pyplot as plt
+#!/usr/bin/env python
+
 import numpy as np
+from matplotlib import pyplot as plt
+from scipy.special import comb
 
-plt.style.use('_mpl-gallery')
 
-# make data
-x = np.linspace(0, 10, 100)
-y = 4 + 2 * np.sin(2 * x)
+def bernstein(i, n, t):
+    return comb(n, i) * (t ** (n - i)) * (1 - t) ** i
 
-# plot
-fig, ax = plt.subplots()
 
-ax.plot(x, y, linewidth=2.0)
+def bezier_curve(nodes, mesh_size = 100):
+    node_x = np.array([n[0] for n in nodes])
+    node_y = np.array([n[1] for n in nodes])
+    t = np.linspace(0.0, 1.0, mesh_size)
+    numerator = np.array(
+        [bernstein(i, len(nodes) - 1, t) for i in
+            range(0, len(nodes))])
+    p_x = np.dot(node_x, numerator)
+    p_y = np.dot(node_y, numerator)
+    return p_x, p_y
 
-ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
-       ylim=(0, 8), yticks=np.arange(1, 8))
 
-plt.show()
+def weighted_bezier_curve(nodes, weights, mesh_size = 100):
+    node_x = np.array([n[0] for n in nodes])
+    node_y = np.array([n[1] for n in nodes])
+    weights = np.array(weights)
 
-def rational_bezier_curv(w, control_point, n):
-    numerator = 0
-    denominator = 0
+    t = np.linspace(0.0, 1.0, mesh_size)
+    weighted_bernstein = np.array(
+        [bernstein(i, len(nodes) - 1, t) * weights[i] for i in
+         range(0, len(nodes))])
 
-    for i in range(0, n+1):
-        numerator += w[i] * control_point
+    sum_weighted_bernstein = np.sum(weighted_bernstein, axis = 0)
+
+    p_x = np.divide(np.dot(node_x, weighted_bernstein), sum_weighted_bernstein)
+    p_y = np.divide(np.dot(node_y, weighted_bernstein), sum_weighted_bernstein)
+    return p_x, p_y
+
+
+if __name__ == '__main__':
+    # Define nodes & weights
+    nodes = [
+        [39.5, 10.5], [30, 20],    #0, 1
+        [6, 6], [13, -12],         #2, 3
+        [63, -12.5], [18.5, 17.5], #4, 5
+        [48, 63], [7, 25.5],       #6, 7
+        [48.5, 49.5], [9, 19.5],   #8, 9
+        [48.5, 35.5], [59, 32.5],  #10, 12
+        [56, 20.5]                 #12
+        ]
+    
+    #Tak jak w poleceniu
+    weights = [1, 2, 3, 2.5, 6, 1.5, 5, 1, 2, 1, 3, 5, 1]
+    p_x, p_y = weighted_bezier_curve(nodes, weights)
+    plt.plot(p_x, p_y, 'g-')
+
+
+
+    # zmianiam wagę p.10: 3 -> 10
+    # weights = [1, 2, 3, 2.5, 6, 1.5, 5, 1, 2, 1, 10, 5, 1]
+
+    #p7: 1 -> 10
+    weights = [1, 2, 3, 2.5, 6, 1.5, 5, 10, 2, 1, 3, 5, 1]
+
+    p_x, p_y = weighted_bezier_curve(nodes, weights)
+    plt.plot(p_x, p_y, 'r-')
+
+
+    #Bez wag
+    # p_x, p_y = bezier_curve(nodes)
+    # plt.plot(p_x, p_y, 'b-')
+
+
+
+    nodes_x = [n[0] for n in nodes]
+    nodes_y = [n[1] for n in nodes]
+    dot_size = [1 + w ** 2 for w in weights]
+    plt.scatter(nodes_x, nodes_y, c = 'k', s = dot_size)
+
+    for n in range(len(nodes)):
+        plt.text(nodes_x[n], nodes_y[n], n)
+
+    plt.show()
