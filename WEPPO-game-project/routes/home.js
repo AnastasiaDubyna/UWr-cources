11c53739ad9bbe45fae81d73184c37388e49f594
AnastasiaDubyna <nastyaorlova5@gmail.com>
2022-03-15 12:44:44 +0100

    weppo project moved to separate repository

--- WEPPO-game-project/routes/home.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require("../controllers/homeController");
-
-router.get("/", controller.renderHomePage);
-
-router.post("/statistics", controller.postStatistics);
-
-module.exports = router;
